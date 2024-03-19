4bb28a65428e467673604839ef80e55b67164db7
Canzova <nihalsingh2082002@gmail.com>
2024-03-19 12:48:13 +0530

    Made Swiper responsive

--- src/Components/Candidates.js
+++ src/Components/Candidates.js
@@ -31,168 +31,181 @@ const Candidates = () => {
           journey.
         </div>
       </div>
-     
-  <div className="testimonials_slider_wrapper cards_wrapper">
-    <Swiper
-      slidesPerView={3}
-      spaceBetween={50}
-      loop={true}
-      pagination={{
-        clickable: true,
-      }}
-      navigation={true}
-      modules={[Pagination, Navigation]}
-      className="mySwiper"
-    >
-      <SwiperSlide>
-        <div className="card_wrapper">
-          <div className="img_container">
-            <img src={temp} alt="" />
-          </div>
-          <div className="card_content">
-            <h3>Preparing for your Next Job</h3>
-            <div>
-              While your work experience sharpens your skills in your field,
-              getting ready for your next career move, demands an additional
-              level of preparation. We're here to assist you in acquiring that
-              competitive advantage through the services listed below:
+      <div className="testimonials_slider_wrapper cards_wrapper" >
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={50}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+          breakpoints={{
+            // Define breakpoints for different screen sizes
+            0: {
+              slidesPerView: 1, // Show 1 card per view on smaller screens
+              
+            },
+            865: {
+              slidesPerView: 2, // Show 2 cards per view on medium-sized screens
+            },
+            1200: {
+              slidesPerView: 3, // Show 3 cards per view on larger screens
+            },
+          }}
+        >
+          <SwiperSlide>
+            <div className="card_wrapper">
+              <div className="img_container">
+                <img src={temp} alt="" />
+              </div>
+              <div className="card_content">
+                <h3>Preparing for your Next Job</h3>
+                <div>
+                  While your work experience sharpens your skills in your field,
+                  getting ready for your next career move, demands an additional
+                  level of preparation. We're here to assist you in acquiring
+                  that competitive advantage through the services listed below:
+                </div>
+                <ul className="overLay">
+                  <li>
+                    <span>Resume Services:</span> Get your resume critiqued or
+                    professionally written by our experts.
+                  </li>
+                  <li>
+                    <span>Online Profiles:</span> Enhance your professional
+                    image with a sophisticated LinkedIn profile.
+                  </li>
+                  <li>
+                    <span>Ace the Interview:</span> Refine your interview
+                    proficiency with our mock interview sessions.
+                  </li>
+                </ul>
+              </div>
             </div>
-            <ul className="overLay">
-              <li>
-                <span>Resume Services:</span> Get your resume critiqued or
-                professionally written by our experts.
-              </li>
-              <li>
-                <span>Online Profiles:</span> Enhance your professional image
-                with a sophisticated LinkedIn profile.
-              </li>
-              <li>
-                <span>Ace the Interview:</span> Refine your interview
-                proficiency with our mock interview sessions.
-              </li>
-            </ul>
-          </div>
-        </div>
-      </SwiperSlide>
+          </SwiperSlide>
 
-      <SwiperSlide>
-        <div className="card_wrapper">
-          <div className="img_container">
-            <img src={temp2} alt="" />
-          </div>
-          <div className="card_content">
-            <h3>Discovering your next job</h3>
-            <div>
-              Discovering a position where your passion, skills, and values
-              converge is undoubtedly vital for a fulfilling career. We promise
-              to 'hold the torch out' for you, with the below services:
+          <SwiperSlide>
+            <div className="card_wrapper">
+              <div className="img_container">
+                <img src={temp2} alt="" />
+              </div>
+              <div className="card_content">
+                <h3>Discovering your next job</h3>
+                <div>
+                  Discovering a position where your passion, skills, and values
+                  converge is undoubtedly vital for a fulfilling career. We
+                  promise to 'hold the torch out' for you, with the below
+                  services:
+                </div>
+                <ul className="overLay">
+                  <li>
+                    <span>Recruitment Consultation:</span> Browse through the
+                    current job opportunities (link) offered by our corporate
+                    clients.
+                  </li>
+                  <li>
+                    <span>Advisory Services:</span> Benefit from our decades of
+                    experience to boost your chances of landing that 'dream job
+                    '.
+                  </li>
+                </ul>
+              </div>
             </div>
-            <ul className="overLay">
-              <li>
-                <span>Recruitment Consultation:</span> Browse through the
-                current job opportunities (link) offered by our corporate
-                clients.
-              </li>
-              <li>
-                <span>Advisory Services:</span> Benefit from our decades of
-                experience to boost your chances of landing that 'dream job '.
-              </li>
-            </ul>
-          </div>
-        </div>
-      </SwiperSlide>
+          </SwiperSlide>
 
-      <SwiperSlide>
-        <div className="card_wrapper">
-          <div className="img_container">
-            <img src={temp3} alt="" />
-          </div>
-          <div className="card_content">
-            <h3>Navigating job offers</h3>
-            <div>
-              Choosing the right path is as important as arriving at the
-              destination. We stand by your side, offering the services listed
-              below:
+          <SwiperSlide>
+            <div className="card_wrapper">
+              <div className="img_container">
+                <img src={temp3} alt="" />
+              </div>
+              <div className="card_content">
+                <h3>Navigating job offers</h3>
+                <div>
+                  Choosing the right path is as important as arriving at the
+                  destination. We stand by your side, offering the services
+                  listed below:
+                </div>
+                <ul className="overLay">
+                  <li>
+                    <span>Offer Evaluation:</span> Receive support in assessing
+                    job offers.
+                  </li>
+                  <li>
+                    <span>Negotiation Strategies:</span>Engage with us to
+                    strategize on optimizing your offers.
+                  </li>
+                  <li>
+                    <span>Comparing Multiple Offers:</span> Receive expert
+                    guidance for a comprehensive, side-by-side evaluation of
+                    multiple offers.
+                  </li>
+                </ul>
+              </div>
             </div>
-            <ul className="overLay">
-              <li>
-                <span>Offer Evaluation:</span> Receive support in assessing job
-                offers.
-              </li>
-              <li>
-                <span>Negotiation Strategies:</span>Engage with us to strategize
-                on optimizing your offers.
-              </li>
-              <li>
-                <span>Comparing Multiple Offers:</span> Receive expert guidance
-                for a comprehensive, side-by-side evaluation of multiple offers.
-              </li>
-            </ul>
-          </div>
-        </div>
-      </SwiperSlide>
+          </SwiperSlide>
 
-      <SwiperSlide>
-        <div className="card_wrapper">
-          <div className="img_container">
-            <img src={temp4} alt="" />
-          </div>
-          <div className="card_content">
-            <h3>Career Decision Points</h3>
-            <div>
-              Steer your career with purpose and efficiency. Seeking guidance?
-              We're available with the services listed below:
+          <SwiperSlide>
+            <div className="card_wrapper">
+              <div className="img_container">
+                <img src={temp4} alt="" />
+              </div>
+              <div className="card_content">
+                <h3>Career Decision Points</h3>
+                <div>
+                  Steer your career with purpose and efficiency. Seeking
+                  guidance? We're available with the services listed below:
+                </div>
+                <ul className="overLay">
+                  <li>
+                    <span>Stay or Pivot:</span>Gain valuable insights to
+                    determine whether to remain in your current position or
+                    explore new opportunities.
+                  </li>
+                  <li>
+                    <span>Exploring New Avenues</span>Discover the possibilities
+                    of a career change with our support.
+                  </li>
+                </ul>
+              </div>
             </div>
-            <ul className="overLay">
-              <li>
-                <span>Stay or Pivot:</span>Gain valuable insights to determine
-                whether to remain in your current position or explore new
-                opportunities.
-              </li>
-              <li>
-                <span>Exploring New Avenues</span>Discover the possibilities of
-                a career change with our support.
-              </li>
-            </ul>
-          </div>
-        </div>
-      </SwiperSlide>
+          </SwiperSlide>
 
-      <SwiperSlide>
-        <div className="card_wrapper">
-          <div className="img_container">
-            <img src={temp5} alt="" />
-          </div>
-          <div className="card_content">
-            <h3>Crafting your Career Trajectory </h3>
-            <div>
-              Regrettably, many individuals tend to opt for conventional career
-              paths by default. We aim to break you out of this default mindset
-              and assist you in gaining a deeper understanding of yourself, to
-              construct a career where you can truly flourish. Our services are
-              customised precisely for this purpose:
+          <SwiperSlide>
+            <div className="card_wrapper">
+              <div className="img_container">
+                <img src={temp5} alt="" />
+              </div>
+              <div className="card_content">
+                <h3>Crafting your Career Trajectory </h3>
+                <div>
+                  Regrettably, many individuals tend to opt for conventional
+                  career paths by default. We aim to break you out of this
+                  default mindset and assist you in gaining a deeper
+                  understanding of yourself, to construct a career where you can
+                  truly flourish. Our services are customised precisely for this
+                  purpose:
+                </div>
+                <ul className="overLay">
+                  <li>
+                    <span>Finding your IKIGAI (link):</span>Discover your
+                    purpose and passion with us.
+                  </li>
+                  <li>
+                    <span>Creating a long-term career-arc:</span>Collaborate
+                    with us to sketch out a long-term career trajectory spanning
+                    several decades.
+                  </li>
+                </ul>
+              </div>
             </div>
-            <ul className="overLay">
-              <li>
-                <span>Finding your IKIGAI (link):</span>Discover your purpose
-                and passion with us.
-              </li>
-              <li>
-                <span>Creating a long-term career-arc:</span>Collaborate with us
-                to sketch out a long-term career trajectory spanning several
-                decades.
-              </li>
-            </ul>
-          </div>
-        </div>
-      </SwiperSlide>
-    </Swiper>
-
-</div>;
-
-      
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      ;
     </div>
   );
 };
 export default Candidates;
-
