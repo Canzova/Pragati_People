156e5864c1de50ef60ee0fb72b37eb68a5e93a15
Canzova <nihalsingh2082002@gmail.com>
2024-03-25 16:33:45 +0530

    Fixed BG problem in mobile

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,20 @@
+import what from "../images/What.png"
+import who from "../images/Who.png"
 const Home = ()=>{
     return(
         <div className="home_wrapper">
             <div className="img_wrapper">
                <h1>Propelling people's progress </h1>
+            </div>
+            <div className="our_services">
+                <div className="who">
+                    <img src={who} alt="" />
+                    <h1>Who we are ?</h1>
+                </div>
+                <div className="what">
+                    <img src={what} alt="" />
+                    <h1>What we do ?</h1>
+                </div>
             </div>
         </div>
     )
