9eadce368578d35046c186c5964aa362c8b72639
Canzova <nihalsingh2082002@gmail.com>
2024-03-26 17:54:28 +0530

    Worked on home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,22 +1,27 @@
-import what from "../images/What.png"
-import who from "../images/Who.png"
-const Home = ()=>{
-    return(
-        <div className="home_wrapper">
-            <div className="img_wrapper">
-               <h1>Propelling people's progress </h1>
-            </div>
-            <div className="our_services">
-                <div className="who">
-                    <img src={who} alt="" />
-                    <h1>Who we are ?</h1>
-                </div>
-                <div className="what">
-                    <img src={what} alt="" />
-                    <h1>What we do ?</h1>
-                </div>
-            </div>
+import home1 from "../images/home-1.jpg";
+import home2 from "../images/home-2.jpg";
+const Home = () => {
+  return (
+    <div className="home_wrapper">
+      <div className="img_wrapper">
+        <h1 className="propelling">Propelling</h1>
+        <h1 className="progress">progress</h1>
+      </div>
+      <div className="our_services">
+        <div className="who">
+          <img src={home1} alt="" />
+          <div className="upper_layer">
+            <h1>Who we are ?</h1>
+          </div>
         </div>
-    )
-}
+        <div className="what">
+          <img src={home2} alt="" />
+          <div className="upper_layer">
+            <h1>What we do ?</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Home;
